f0ece8374f3861b42b503fd902bcd6333df2a4d3
codefiles024 <codefiles88@gmail.com>
2019-07-30 20:20:20 +0800

    Testing app purposes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,81 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+   <div id="app">
+    <div class="header-bar right-content">
+		<div class="container">
+			<div class="skip-link screen-reader-text" href="#site-content">Skip to content</div>
+			
+<form role="search" method="get" class="search-form" action="http://mpbl.web.geniussports.com/">
+	<label>
+		<span class="screen-reader-text">Search for:</span>
+		<input type="search" class="search-field" placeholder="Search" title="Press Enter to submit your search" value="" name="s">
+	</label>
+	<input type="submit" class="search-submit" value="Search">
+</form>
+								</div>
+	</div>
+  <div class="site-header-main">
+		<div class="container">
+			<div class="site-branding">
+					<div class="custom-logo-container">
+					<div class="custom-logo">
+					<a href="http://mpbl.web.geniussports.com/" rel="home"></a>
+					</div>
+				  </div>
+					<h2 class="site-title">
+					<a href="http://mpbl.web.geniussports.com/" rel="home"><h2>Maharlika Pilipinas Basketball League</h2></a>
+				  </h2>
+											</div>
+
+			<div class="header-social-logos">
+        <section id="lab_social_icon_footer">
+<!-- Include Font Awesome Stylesheet in Header -->
+      <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+      <div class="container">
+      <div class="text-center center-block">
+     </div>
+     </div>
+    </section>
+							<ul class="social-customizer social-links footer-social-links">
+									<li class="facebook-official">
+						<a href="https://www.facebook.com/mpblofficialph/">
+							<i class="fa fa-fw fa-facebook-square">
+								<span>Facebook</span>
+							</i>
+						</a>
+					</li>
+									<li class="twitter">
+						<a href="https://twitter.com/officialmpbl">
+							<i class="fa fa-fw fa-twitter-square">
+								<span>Twitter</span>
+							</i>
+						</a>
+					</li>
+									<li class="instagram">
+						<a href="http://www.instagram.com/officialmpbl">
+							<i class="fa fa-fw fa-instagram">
+								<span>Instagram</span>
+							</i>
+						</a>
+					</li>
+								</ul>
+						</div>
+		</div>
+	</div>
+  <nav id="site-navigation" class="site-navigation" role="navigation">
+		<span class="menu-toggle">Menu</span>
+		<a class="skip-link screen-reader-text" href="#site-content">Skip to content</a>
+		<div class="menu-main-menu-container">
+      <ul id="menu-main-menu" class="menu">
+        <li class="current-menu-item"><router-link to="/">Home</router-link></li>
+          <li ><a href="/competitions/?cu=MPBL/schedule" title="Schedule">Schedule</a></li>
+          <li ><a href="/competitions/?cu=MPBL/standings" title="Standings">Standings</a></li>
+          <li ><a href="/competitions/?cu=MPBL/leaders" title="Leaders">Leaders</a></li>
+           <li > <router-link to="/about">About</router-link></li>
+          <li class="drop-menu">
+          <a href="#" title="Menu">Menu</a>
+          <ul class="sub-menu"></ul>
+          </li></ul></div>	</nav>
+          
     <router-view />
   </div>
 </template>
@@ -27,5 +99,111 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+.wrapper {
+  width: 1500px;
+  margin: auto;
+  border: 1px solid #333;
+}
+.block {
+  background-color: #F5F5F5;
+  color: #008080;
+  height: 100px;
+  text-align: center;
+  padding: 20px
+}
+.horizontal-scroll {
+  background-color: rgb(138, 136, 136);
+  overflow: hidden;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  white-space: nowrap;
+  padding: 30px 0;
+}
+.item {
+  border: 1px solid rgb(0, 0, 0);
+  display: inline-block;
+  width: 150px;
+  background-color: rgb(5, 4, 4);
+  text-align: center;
+  padding: 50px 30px;
+  font-size: 20px;
+  margin: 0 10px
+}
+footer {
+  color: #F5F5F5;
+  text-align: left;
+  background-color: #008080;
+  padding: 20px;
+  font-size: 12px;
+}
+.skip-link screen-reader-text{
+  background-color: #42b983;
+}
+
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: rgb(20, 90, 243);
+  color: white;
+}
+#lab_social_icon_footer {
+  padding: 40px 0;
+  background-color: #0955a0;
+}
+
+#lab_social_icon_footer a {
+  color: #2c3e50;
+}
+
+#lab_social_icon_footer .social:hover {
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -o-transform: scale(1.1);
+}
+
+#lab_social_icon_footer .social {
+  -webkit-transform: scale(0.8);
+  /* Browser Variations: */
+  
+  -moz-transform: scale(0.8);
+  -o-transform: scale(0.8);
+  -webkit-transition-duration: 0.5s;
+  -moz-transition-duration: 0.5s;
+  -o-transition-duration: 0.5s;
+}
+/*
+    Multicoloured Hover Variations
+*/
+
+#lab_social_icon_footer #social-fb:hover {
+  color: rgb(255, 255, 255);
+}
+
+#lab_social_icon_footer #social-tw:hover {
+  color: rgb(255, 255, 255);
+}
+
+#lab_social_icon_footer #social-gp:hover {
+  color: #d34836;
+}
+
+#lab_social_icon_footer #social-em:hover {
+  color: #f39c12;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,158 @@
 <template>
+   <div id="app">
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div id="app">
+    <div class="home">
+        <div class="standings"><h1>STANDINGS</h1>
+          <div class="post" v-for="post in posts" :key="post.id">
+          <div class="scorePerGame" >
+            <table class="table table-dark">
+              <thead>
+                <tr>
+                  <th scope="col" class="font">ID</th>
+                  <th scope="col"  class="font">Team</th>
+                  <th scope="col" class="font"></th>
+                  <th scope="col" class="font">Win</th>
+                  <th scope="col" class="font">Lose</th>
+                  <th scope="col" class="font" >Location</th>
+                  <th scope="col" class="font">Team Leader</th>
+                  <th scope="col" class="font">Team Manager</th>
+
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="p in post.data" :key="p.id">
+                  <th class="align"> {{p.id}}</th>
+                  <td class="align"><img :src="p.image.url" width="100%" v-if="p.image.url" /></td>
+                  <td class="align">{{p.name}}</td>
+                  <td class="align">{{p.win}}</td>
+                  <td class="align">{{p.lose}}</td>
+                  <td class="align">{{p.location_preference}}</td>
+                  <td class="align">{{p.team_leader}}</td>
+                  <td class="align">{{p.team_manager}}</td>
+                </tr>
+              </tbody>
+            </table>
+
+          </div>
+        </div>
+  </div>
+  </div>
+  </div>
+  <nav id="site-navigation" class="site-navigation" role="navigation">
+		<!-- <span class="menu-toggle">Menu</span> -->
+		<a class="skip-link screen-reader-text" href="#site-content">Skip to content</a>
+		<div class="menu-main-menu-container">
+      <ul id="menu-main-menu" class="menu">
+        <!-- <li class="current-menu-item"></li> -->
+          <li ><a href="/competitions/?cu=MPBL/schedule" title="Schedule">SCHEDULE</a></li>
+          <li ><a href="/competitions/?cu=MPBL/standings" title="Standings">STANDINGS</a></li>
+          <li ><a href="/competitions/?cu=MPBL/leaders" title="Leaders">LEADERS</a></li>
+          <li ><a href="/competitions/?cu=MPBL/statistics" title="statistics">STATISTICS</a></li>
+          <li ><a href="/competitions/?cu=MPBL/teams" title="teams">TEAMS</a></li>
+          <li ><a href="/competitions/?cu=MPBL/players" title="players">PLAYERS</a></li>
+
+          <!-- <li class="drop-menu">
+          <a href="#" title="Menu">Menu</a>
+          <ul class="sub-menu"></ul> -->
+          </li></ul></div>	</nav>
+        <section class="block">This is a content</section>
+        <footer id="site-footer" class="site-footer footer-layout-1" role="contentinfo">
+        <div class="container">
+        <div class="site-info">
+        <span class="site-copyright">&copy; Genius Sports Group. All content responsibility of site administrator.</span>
+        <span class="global-logo">
+        <a title="Genius Sports Group" href="http://geniussports.com/">
+            Genius Sports Group
+        </a>
+        </span>
+        </div>
+        </div>
+        </footer>	
+        </div>
   </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from 'axios'
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
+  data(){
+    return{
+      posts:[]
+    }
+  },
+
+  mounted(){
+    var self = this;
+    axios.get('http://api.crossovrapp.com/v3/mpbl/teams')
+    .then(function(res){
+      self.posts =  res.data;
+      console.log('Data:', res.data);
+  })
+  .catch(function(error){
+    console.log('Error: ', error);
+  })
   }
-};
+}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#app {
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  background: #f8f6f6;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#nav {
+  padding: px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
-a {
+
+#nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.post {
+  background-color: rgb(253, 252, 252);
+   text-align: left;
+  width: 600px;
+  height: 400px;
+}
+.scorePerGame {
+  background-color: rgb(221, 221, 221);
+  margin: 50px;
+  width: 500px;
+  border-style: dotted;
+ text-align: center;
+}
+.standings{
+   margin: 60px;
+   color: rgb(122, 122, 122);
+   font-family:Georgia, 'Times New Roman', Times, serif;
+   font-style:Bold;
+
+}
+.align{
+  text-align: center;
+  border-radius: 8px;
+  font-size: 13px;
+}
+.font{
+    font-size: 13px;
+    font-style: Bold;
+}
+.block{
+  background: #c4c4c4;
 }
 </style>
